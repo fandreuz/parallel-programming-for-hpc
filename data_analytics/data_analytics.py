2c81117a9abb0f2a3283c365d8c451d85c21ae65
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-04-07 09:55:04 +0200

    minor update

--- data_analytics/data_analytics.py
+++ data_analytics/data_analytics.py
@@ -45,42 +45,50 @@ size = args.size
 plt.figure(figsize=(20, 6))
 
 comm_preps_0, comm_0, comp_0, gpu_0 = process_pair(0, size)
-# comm_preps_1, comm_1, comp_1 = process_pair(1, size)
 comm_preps_2, comm_2, comp_2, gpu_2 = process_pair(2, size)
 comm_preps_3, comm_3, comp_3, gpu_3 = process_pair(3, size)
 
 width = 0.25
 
-plt.bar(mpi_procs, comm_0, width, label="Naive (comm)")
-plt.bar(mpi_procs, comp_0, width, bottom=comm_0, label="Naive (comp)")
+comm_line_0 = plt.bar(mpi_procs, comm_0, width, color='r', edgecolor='k')
+comp_line_0 = plt.bar(mpi_procs, comp_0, width, bottom=comm_0, color='b', edgecolor='k')
 
-plt.bar(mpi_procs + width, comm_2, width, label="DGEMM (comm)")
-plt.bar(mpi_procs + width, comp_2, width, bottom=comm_2, label="DGEMM (comp)")
+plt.bar(mpi_procs + width, comm_2, width, color='r', edgecolor='k')
+comp_line_2 = plt.bar(mpi_procs + width, comp_2, width, bottom=comm_2, color='b', edgecolor='k')
 
-plt.bar(mpi_procs + 2 * width, comm_3, width, label="cuBLAS DGEMM (comm)")
-plt.bar(
+plt.bar(mpi_procs + 2 * width, comm_3, width, color='r', edgecolor='k')
+gpu_line_3 = plt.bar(
     mpi_procs + 2 * width,
     gpu_3,
     width,
     bottom=comm_3,
-    label="cuBLAS DGEMM (GPU transfer)",
+    color='g',
+    edgecolor='k'
 )
-plt.bar(
+comp_line_3 = plt.bar(
     mpi_procs + 2 * width,
     comp_3,
     width,
     bottom=comm_3 + gpu_3,
-    label="cuBLAS DGEMM (comp)",
+    color='b',
+    edgecolor='k'
 )
 
 plt.xticks(mpi_procs, mpi_procs)
 plt.xlabel("MPI processes (1 per node)")
 plt.ylabel("Seconds")
 
+def add_label(bars, label):
+	target = bars[0]
+	plt.text(target.xy[0], target.xy[1] + target._height, label, fontsize=10)
+add_label(comp_line_0, "Naive")
+add_label(comp_line_2, "OpenBLAS")
+add_label(comp_line_3, "cuBLAS")
+
 plt.yscale("log")
 
 plt.title(f"{size} x {size} matmul")
 
-plt.legend()
+plt.legend((comm_line_0, gpu_line_3, comp_line_0), ("Communication", "Host-device transfer", "Computation"))
 
 plt.show()
