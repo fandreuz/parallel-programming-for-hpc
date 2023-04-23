3fb7c2fd0dd9ea32106fcd37eca59b1332e0f6fb
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-04-24 01:41:29 +0200

    cleanup

--- jacobi/code/jacobi.c
+++ jacobi/code/jacobi.c
@@ -5,18 +5,9 @@
 #include <sys/time.h>
 #include <time.h>
 
-/*** function declarations ***/
-
-// save matrix to file
 void save_gnuplot(double *M, size_t dim);
 
-// evolve Jacobi
 void evolve(double *matrix, double *matrix_new, size_t dimension);
-
-// return the elapsed time
-double seconds(void);
-
-/*** end function declaration ***/
 
 int main(int argc, char *argv[]) {
   int myRank, nProcesses;
@@ -24,21 +15,16 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
   MPI_Comm_size(MPI_COMM_WORLD, &nProcesses);
 
-  // timing variables
-  double t_start, t_end, increment;
-
-  // indexes for loops
-  size_t i, j, it;
-
-  // initialize matrix
-  double *matrix, *matrix_new, *tmp_matrix;
-
   size_t dimension = 0, iterations = 0, row_peek = 0, col_peek = 0;
   size_t byte_dimension = 0;
 
-  // check on input parameters
   if (argc != 5) {
-    fprintf(stderr, "\nwrong number of arguments. Usage: ./a.out dim it n m\n");
+    if (myRank == 0) {
+      fprintf(stderr,
+              "\nwrong number of arguments. Usage: ./a.out dim it n m\n");
+    }
+
+    MPI_Finalize();
     return 1;
   }
 
@@ -47,16 +33,25 @@ int main(int argc, char *argv[]) {
   row_peek = atoi(argv[3]);
   col_peek = atoi(argv[4]);
 
-  printf("matrix size = %zu\n", dimension);
-  printf("number of iterations = %zu\n", iterations);
-  printf("element for checking = Mat[%zu,%zu]\n", row_peek, col_peek);
+  if (myRank == 0) {
+    printf("matrix size = %zu\n", dimension * nProcesses);
+    printf("number of iterations = %zu\n", iterations);
+    printf("element for checking = Mat[%zu,%zu]\n", row_peek, col_peek);
+  }
 
   if ((row_peek > dimension) || (col_peek > dimension)) {
-    fprintf(stderr,
-            "Cannot Peek a matrix element outside of the matrix dimension\n");
-    fprintf(stderr, "Arguments n and m must be smaller than %zu\n", dimension);
+    if (myRank == 0) {
+      fprintf(stderr,
+              "Cannot Peek a matrix element outside of the matrix dimension\n");
+      fprintf(stderr, "Arguments n and m must be smaller than %zu\n",
+              dimension); /*  */
+    }
+
+    MPI_Finalize();
     return 1;
   }
+
+  double *matrix, *matrix_new;
 
   byte_dimension = sizeof(double) * (dimension + 2) * (dimension + 2);
   matrix = (double *)malloc(byte_dimension);
@@ -65,15 +60,15 @@ int main(int argc, char *argv[]) {
   memset(matrix, 0, byte_dimension);
   memset(matrix_new, 0, byte_dimension);
 
-  // fill initial values
-  for (i = 1; i <= dimension; ++i)
-    for (j = 1; j <= dimension; ++j)
+  // initial values
+  for (size_t i = 1; i <= dimension; ++i)
+    for (size_t j = 1; j <= dimension; ++j)
       matrix[(i * (dimension + 2)) + j] = 0.5;
 
-  // set up borders
-  increment = 100.0 / (dimension + 1);
+  // borders
+  double increment = 100.0 / (dimension + 1);
 
-  for (i = 1; i <= dimension + 1; ++i) {
+  for (size_t i = 1; i <= dimension + 1; ++i) {
     matrix[i * (dimension + 2)] = i * increment;
     matrix[((dimension + 1) * (dimension + 2)) + (dimension + 1 - i)] =
         i * increment;
@@ -82,20 +77,17 @@ int main(int argc, char *argv[]) {
         i * increment;
   }
 
-  // start algorithm
-  t_start = seconds();
-  for (it = 0; it < iterations; ++it) {
-
+  double t_start = MPI_Wtime()();
+  for (size_t it = 0; it < iterations; ++it) {
     evolve(matrix, matrix_new, dimension);
 
-    // swap the pointers
-    tmp_matrix = matrix;
+    double *tmp_matrix = matrix;
     matrix = matrix_new;
     matrix_new = tmp_matrix;
   }
-  t_end = seconds();
+  double t_end = MPI_Wtime()();
 
-  printf("\nelapsed time = %f seconds\n", t_end - t_start);
+  printf("\nelapsed time = %f MPI_Wtime()\n", t_end - t_start);
   printf("\nmatrix[%zu,%zu] = %f\n", row_peek, col_peek,
          matrix[(row_peek + 1) * (dimension + 2) + (col_peek + 1)]);
 
@@ -110,12 +102,8 @@ int main(int argc, char *argv[]) {
 }
 
 void evolve(double *matrix, double *matrix_new, size_t dimension) {
-
-  size_t i, j;
-
-  // This will be a row dominant program.
-  for (i = 1; i <= dimension; ++i)
-    for (j = 1; j <= dimension; ++j)
+  for (size_t i = 1; i <= dimension; ++i)
+    for (size_t j = 1; j <= dimension; ++j)
       matrix_new[(i * (dimension + 2)) + j] =
           (0.25) * (matrix[((i - 1) * (dimension + 2)) + j] +
                     matrix[(i * (dimension + 2)) + (j + 1)] +
@@ -123,28 +111,16 @@ void evolve(double *matrix, double *matrix_new, size_t dimension) {
                     matrix[(i * (dimension + 2)) + (j - 1)]);
 }
 
+const double h = 0.1;
 void save_gnuplot(double *M, size_t dimension) {
-
-  size_t i, j;
-  const double h = 0.1;
   FILE *file;
 
   file = fopen("solution.dat", "w");
 
-  for (i = 0; i < dimension + 2; ++i)
-    for (j = 0; j < dimension + 2; ++j)
+  for (size_t i = 0; i < dimension + 2; ++i)
+    for (size_t j = 0; j < dimension + 2; ++j)
       fprintf(file, "%f\t%f\t%f\n", h * j, -h * i,
               M[(i * (dimension + 2)) + j]);
 
   fclose(file);
-}
-
-// A Simple timer for measuring the walltime
-double seconds() {
-
-  struct timeval tmp;
-  double sec;
-  gettimeofday(&tmp, (struct timezone *)0);
-  sec = tmp.tv_sec + ((double)tmp.tv_usec) / 1000000.0;
-  return sec;
 }
