88e06ba24c5f6d5441083398686fd95129149bac
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-04-26 21:48:20 +0200

    black

--- jacobi/plot_jacobi.py
+++ jacobi/plot_jacobi.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("solution.dat")[:,2]
+data = np.loadtxt("solution.dat")[:, 2]
 data = data.reshape(int(np.sqrt(len(data))), -1)
 
 plt.figure(figsize=(8, 6))
