a71c8a897befc787c55f0840373c60fd1eaf084d
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-05-01 14:57:18 +0200

    cleanup imports

--- fft/fft_3d.h
+++ fft/fft_3d.h
@@ -1,5 +1,4 @@
 #include "utilities.h"
-#include <stdlib.h>
 
 void setup_fft3d(struct Fft3dInfo *info, int n1, int n2, int n3);
 void fft_3d_2(double *data, fftw_complex *out, struct Fft3dInfo *fft_3d_info);
