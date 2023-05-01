ec1aabff0e0fb9e6d0d236236a0c7ea3db7df9cd
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-05-02 01:38:42 +0200

    rectify

--- fft/fft_3d.c
+++ fft/fft_3d.c
@@ -162,7 +162,7 @@ void fft_3d_2(double *data, fftw_complex *out, struct Fft3dInfo *fft_3d_info) {
   send_split(fft_3d_info->fft_2d_in, fft_3d_info->fft_1d_out, fft_3d_info);
   rectify_3(fft_3d_info->fft_1d_out, fft_3d_info->fft_1d_in,
             fft_3d_info->loc_n3, fft_3d_info->n2, fft_3d_info->n1,
-            fft_3d_info->nProcesses);
+            ffcomt_3d_info->axis1_counts, fft_3d_info->nProcesses);
 
   fftw_execute(fft_3d_info->fft_1d_many);
 
@@ -170,7 +170,8 @@ void fft_3d_2(double *data, fftw_complex *out, struct Fft3dInfo *fft_3d_info) {
            fft_3d_info->n2, fft_3d_info->n1);
   send_split_back(fft_3d_info->fft_1d_in, fft_3d_info->fft_2d_in, fft_3d_info);
   rectify_3(fft_3d_info->fft_2d_in, out, fft_3d_info->loc_n1, fft_3d_info->n2,
-            fft_3d_info->n3, fft_3d_info->nProcesses);
+            fft_3d_info->n3, ffcomt_3d_info->axis3_counts,
+            fft_3d_info->nProcesses);
 }
 
 void ifft_3d_2(fftw_complex *data, double *out, struct Fft3dInfo *fft_3d_info) {
