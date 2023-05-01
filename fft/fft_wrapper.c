64aceaedcb407a78e6c35d491bcf38b65343e3dd
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-05-01 17:14:34 +0200

    more cleanup

--- fft/fft_wrapper.c
+++ fft/fft_wrapper.c
@@ -31,22 +31,17 @@ void close_fftw(fftw_mpi_handler *fft) {
 
 void fft_3d(fftw_mpi_handler *fft, double *data_direct, fftw_complex *data_rec,
             bool direct_to_reciprocal) {
-
-  double fac;
-  int i;
-
   if (direct_to_reciprocal) {
     fft_3d_2(data_direct, data_rec, fft->fft_3d_info);
   } else {
-
     memcpy(fft->fftw_data, data_rec,
            fft->local_size_grid * sizeof(fftw_complex));
 
     fftw_mpi_execute_dft(fft->bw_plan, fft->fftw_data, fft->fftw_data);
 
-    fac = 1.0 / fft->global_size_grid;
+    double fac = 1.0 / fft->global_size_grid;
 
-    for (i = 0; i < fft->local_size_grid; ++i) {
+    for (int i = 0; i < fft->local_size_grid; ++i) {
 
       data_direct[i] = creal(fft->fftw_data[i]) * fac;
     }
