f534f3fedcb4e590714effaaee21b103b6ddd304
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-05-01 03:28:00 +0200

    typo

--- fft/fft_3d.h
+++ fft/fft_3d.h
@@ -5,7 +5,7 @@
 struct DataInfo {
     int loc_n1;
     int loc_n1_offset;
-}
+};
 
-int setup_fft_3d(int n1, int n2, int n3);
+DataInfo setup_fft_3d(int n1, int n2, int n3);
 fftw_complex * fft_3d(double *data, int loc_n1, int n2, int n3);
