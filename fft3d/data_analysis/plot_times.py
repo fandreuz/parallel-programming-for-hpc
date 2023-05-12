cab26de403a2ba495921cb7b94243e7fb56f12a7
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-05-12 08:35:01 +0200

    minor

--- fft3d/data_analysis/plot_times.py
+++ fft3d/data_analysis/plot_times.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from string import Template
 
 dirs = ("diffusion_fft", "fft")
-file_patterns = (Template("fftw_${n}.out"), Template("my_${n}.out"))
+file_patterns = (Template("fftw_${n}.out"), Template("my2_${n}.out"))
 procs = np.array((1, 2, 4, 8))
 
 data = {dir: {} for dir in dirs}
@@ -27,14 +27,14 @@ plt.figure(figsize=(20, 6))
 
 width = 0.25
 plt.bar(procs, as_list(data["diffusion_fft"]), width, edgecolor="k", label="FFTW")
-plt.bar(procs + width, as_list(data["fft"]), width, edgecolor="k", label="MPI_Alltoallv")
+plt.bar(
+    procs + width, as_list(data["fft"]), width, edgecolor="k", label="MPI_Alltoallv"
+)
 
 plt.ylabel("Seconds/iteration")
 plt.xlabel("MPI processes")
 
 plt.title("Grid = $512 \\times 512 \\times 512$ (averaged over 100 time steps)")
 plt.legend()
-
-plt.yscale("log")
 
 plt.show()
